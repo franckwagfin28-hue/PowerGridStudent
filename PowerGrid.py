23a27f314520382794d69ca99d14a2000a94d6c2
audreyvm1 <audrey.tchinda-sonwa.etu@univ-rennes.fr>
2025-11-30 12:41:46 +0100

    Final

--- PowerGrid.py
+++ PowerGrid.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     reseau = Reseau()
     terrain = Terrain()
-    terrain.charger("terrains/t1.txt")
+    terrain.charger("terrains/t2.txt")
     print("Terrain chargé :")
     terrain.afficher()
     print("======= Configuration Automatique")
